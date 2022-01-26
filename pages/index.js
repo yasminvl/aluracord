cfe30889c67e3f4058abd23091d94da7be9c9479
Yasmin Benicio <79879086+yasminvl@users.noreply.github.com>
2022-01-26 17:05:01 -0300

    first screen created

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,26 @@
+import React from "react";
+import { useRouter } from "next/router";
+
 import appConfig from "../config.json";
 
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>
-            {`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-              }
-              body {
-                font-family: 'Open Sans', sans-serif;
-              }
-              /* App fit Height */ 
-              html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-              }
-              #__next {
-                flex: 1;
-              }
-              #__next > * {
-                flex: 1;
-              }
-              /* ./App fit Height */ 
-            `}
-        </style>
-    )
+async function getNome(user) {
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    const dados = await response.json()
+    if (dados.message) {
+        return false
+    }
+    return dados.name
+}
+
+async function getLocation(user) {
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    const dados = await response.json()
+    if (dados.message) {
+        return false
+    }
+    return dados.location
 }
 
 function Titulo(props) {
@@ -62,11 +52,39 @@ export default HomePage;
 */
 
 export default function PaginaInicial() {
-    const username = 'yasminvl';
+    //const username = 'yasminvl';
+    //const [username, setUsername] = React.useState('yasminvl');
+    const roteamento = useRouter();
+
+    const [username, setUsername] = React.useState('')
+    const [dirty, setDirty] = React.useState(false)
+    const [nome, setNome] = React.useState('')
+    const [location, setLocation] = React.useState("")
+    const [userlocation, setUserlocation] = React.useState('')
+    const [found, setFound] = React.useState(true)
+
+    function getUrl() {
+        if (!found) {
+            return 'https://hugocalixto.com.br/wp-content/uploads/sites/22/2020/07/error-404-1.png'
+        }
+        if (username.length > 2) {
+            return `https://github.com/${username}.png`
+        }
+        return 'https://i.imgur.com/O1zaVNB.png'
+    }
+
+    const loadUserlocation = async () => {
+        const url = `https://api.github.com/users/${username}`
+        let response = await fetch(url)
+        let json = await response.json()
+        setUserlocation(json.location)
+    }
+    React.useEffect(() => {
+        loadUserlocation()
+    }, [username])
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -94,17 +112,75 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form');
+                            roteamento.push('/chat');
+                            // window.location.href = '/chat';
+                            {/* sem usar o nest seria assim: 
+                                window.location.href = '/chat'; */}
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Titulo tag="h2">Boas vindas!</Titulo>
+                        <Titulo tag="h2">Boas vindas {nome || ''}!</Titulo>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
 
+                        {/*<input 
+                        type="text"
+                        value={yasminvl}
+                        onChange={function (event) {
+                            console.log("usuario")
+                            //onde ta a variavel?
+                            const valor = event.target.value;
+                            // trocar o valor da variavel
+                            // através do react e avise quem precisa
+                            setUsername(valor);
+
+                        }}
+                    />*/}
+
                         <TextField
+                            value={username}
+                            placeholder='GitHub User'
+                            onChange={function (event) {
+
+
+                                console.log('usuario digitou', event.target.value);
+                                //Onde está o valor?
+                                let valor = event.target.value
+                                //Trocar o valor da variável através do React
+                                if (valor.length > 2) {
+                                    getNome(valor).then((e) => {
+                                        if (e) {
+                                            setNome(e)
+                                            setFound(true)
+                                        } else {
+                                            setNome('')
+                                            setFound(false)
+                                        }
+                                    })
+                                } else {
+                                    setNome('')
+                                    setFound(true)
+                                }
+
+                                setUsername(valor)
+                                setDirty(true)
+
+
+                                //AQUI---------
+                                // console.log("usuario")
+                                //onde ta a variavel?
+                                //const valor = event.target.value;
+                                // trocar o valor da variavel
+                                // através do react e avise quem precisa
+                                //setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -115,7 +191,34 @@ export default function PaginaInicial() {
                                 },
                             }}
                         />
+
+
+                        {username.length <= 2 && dirty &&
+                            <Text
+                                styleSheet={{
+                                    marginBottom: '10px', color: appConfig.theme.colors.primary[700],
+                                    fontFamily: 'Play',
+                                }}
+                            >
+                                Digite mais que 2 caracteres
+                            </Text>}
+
+                        {!found &&
+                            <Text
+                                styleSheet={{
+                                    marginBottom: '10px', color: appConfig.theme.colors.primary[700],
+                                    fontFamily: 'Play'
+                                }}>
+                                Usuário inválido
+                            </Text>
+
+                        }
+
+
+
                         <Button
+                            disabled={username.length <= 2 || !found}
+
                             type='submit'
                             label='Entrar'
                             fullWidth
@@ -151,19 +254,39 @@ export default function PaginaInicial() {
                                 borderRadius: '50%',
                                 marginBottom: '16px',
                             }}
-                            src={`https://github.com/${username}.png`}
+                            src={getUrl()}
+                        //src={`https://github.com/${username}.png`}
                         />
                         <Text
                             variant="body4"
                             styleSheet={{
                                 color: appConfig.theme.colors.neutrals[200],
-                                backgroundColor: appConfig.theme.colors.neutrals[900],
+                                backgroundColor: username.length <= 2 || !found
+                                    ? appConfig.theme.colors.neutrals[800]
+                                    : appConfig.theme.colors.primary[500],
                                 padding: '3px 10px',
                                 borderRadius: '1000px'
                             }}
                         >
-                            {username}
+                            <p>{username || 'sem usuario'}</p>
                         </Text>
+
+                        <Text
+                            variant="body4"
+                            styleSheet={{
+                                color: appConfig.theme.colors.neutrals[200],
+                                backgroundColor: username.length <= 2 || !found
+                                    ? appConfig.theme.colors.primary[""]
+                                    : appConfig.theme.colors.neutrals[500],
+                                padding: '3px 10px',
+                                margin: '10px',
+                                borderRadius: '1000px'
+                            }}
+                        >
+                            {userlocation}
+                        </Text>
+
+
                     </Box>
                     {/* Photo Area */}
                 </Box>
